8ae9ddd2c07291da1254698a62d3597f2384d84e
Aaron Ogle <atogle@gmail.com>
2012-09-09 21:37:55 -0400

    - Break single route into its own view - Wire up the add form - Add link to delete a route

--- src/js/baldwin.js
+++ src/js/baldwin.js
@@ -24,15 +24,42 @@ var Baldwin = Baldwin || {};
       this.collection.on('add', this.render, this);
       this.collection.on('remove', this.render, this);
 
-      this.routeTemplate = _.template(
-        '<li class="well well-small">' +
-          '<h2>{{ start }} to {{ end }}</h2>' +
-          '<ol class="unstyled trip-list"></ol>' +
-        '</li>'
-      );
+      this.routeViews = {};
 
       this.noRoutesTemplate = _.template(
         '<li class="alert alert-block alert-info">Add some routes to see the upcoming trips!</li>'
+      );
+    },
+    render: function(){
+      var self = this;
+      this.routeViews = {};
+
+      if (this.collection.size() === 0) {
+        this.$el.html(this.noRoutesTemplate());
+      } else {
+        this.$el.empty();
+        this.collection.each(function(model){
+          self.routeViews[model.cid] = new B.RouteView({ model: model });
+          self.$el.append(self.routeViews[model.cid].render().el);
+        });
+      }
+
+      return this;
+    }
+  });
+
+  B.RouteView = Backbone.View.extend({
+    events: {
+      'click .remove-route': 'removeRoute'
+    },
+
+    initialize: function() {
+      this.routeTemplate = _.template(
+        '<li class="well well-small">' +
+          '<button type="button" class="close remove-route">&times;</button>' +
+          '<h2>{{ start }} to {{ end }}</h2>' +
+          '<ol class="unstyled trip-list"></ol>' +
+        '</li>'
       );
 
       this.tripTemplate = _.template(
@@ -46,46 +73,40 @@ var Baldwin = Baldwin || {};
         '<li class="alert alert-block alert-info">{{ message }}</li>'
       );
     },
-    render: function(){
-      var self = this;
-      if (this.collection.size() === 0) {
-        this.$el.html(this.noRoutesTemplate());
-      } else {
-        this.$el.empty();
-        this.collection.each(function(model){
-          var $routeEl = $(self.routeTemplate(model.toJSON())).appendTo(self.$el);
-          self.renderTrips($routeEl, model);
-        });
-      }
+
+    render: function() {
+      this.$el.html(this.routeTemplate(this.model.toJSON()));
+      this.renderTrips();
 
       return this;
     },
-    renderTrips: function($routeEl, model) {
+
+    renderTrips: function() {
       var self = this;
       $.ajax({
         url: 'http://www3.septa.org/hackathon/NextToArrive/',
         data: {
-          req1: model.get('start'),
-          req2: model.get('end'),
+          req1: this.model.get('start'),
+          req2: this.model.get('end'),
           req3: 3
         },
         dataType: 'jsonp',
         success: function(trips){
           if (trips.length > 0) {
             _.each(trips, function(trip) {
-              self.renderTrip($routeEl, trip);
+              self.renderTrip(trip);
             });
           } else {
-            self.renderMessage($routeEl, 'Sorry, no upcoming trips were found.');
+            self.renderMessage('Sorry, no upcoming trips were found.');
           }
         },
         error: function() {
-          self.renderMessage($routeEl, 'Oops, something went wrong when our ' +
+          self.renderMessage('Oops, something went wrong when our ' +
             'robots were talking to SEPTA\'s robots. Try again in a sec!');
         }
       });
     },
-    renderTrip: function($routeEl, trip) {
+    renderTrip: function(trip) {
       var data = _.extend({}, trip),
           minsLate = parseInt(data.orig_delay, 10);
 
@@ -100,31 +121,57 @@ var Baldwin = Baldwin || {};
         data.status_label_class = 'label-success';
       }
 
-      $routeEl.append(this.tripTemplate(data));
+      this.$('.trip-list').append(this.tripTemplate(data));
 
     },
-    renderMessage: function($routeEl, message) {
-      $routeEl.html(this.messageTemplate({
+    renderMessage: function(message) {
+      this.$el.html(this.messageTemplate({
         message: message
       }));
+    },
+    removeRoute: function() {
+      if (window.confirm('Really delete?')) {
+        this.model.destroy();
+      }
     }
   });
 
 
   // View for adding a trip to the collection
   B.AddRouteView = Backbone.View.extend({
-    initialize: function(){},
-    render: function(){
+    events: {
+      'submit': 'addRoute'
+    },
 
+    getAttrs: function() {
+      var attrs = {};
+
+      // Get values from the form
+      _.each(this.$el.serializeArray(), function(item, i) {
+        attrs[item.name] = item.value;
+      });
+
+      return attrs;
+    },
+
+    addRoute: function(evt) {
+      evt.preventDefault();
+      var attrs = this.getAttrs();
+      this.collection.create(attrs);
     }
   });
 
-  window.routes = new B.RouteList();
+  var routeCollection = new B.RouteList(),
+      addRouteView = new B.AddRouteView({
+        el: '#add-route',
+        collection: routeCollection
+      }),
+      routeListView = new B.RouteListView({
+        el: '#route-list',
+        collection: routeCollection
+      });
 
-  window.routeListView = new B.RouteListView({
-    el: '#route-list',
-    collection: window.routes
-  }).render();
+  routeCollection.fetch();
 
   $('.station').typeahead({source: B.stations});
 })(Baldwin, jQuery);
